9cb358b883d1403593244c240f00340d195deba6
nanc <1438206000@qq.com>
2021-10-30 11:54:55 +0800

    项目初始化,引入vent组件库

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,22 @@
   <div id="app">
     <h1>头条练习</h1>
     <!-- 路由匹配展示视图 -->
-    <router-view/>
+
+    <router-view />
+
+    <!-- 图标 -->
+    <i class="iconfont icon-shipin"></i>
+    <van-cell-group inset>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App",
+};
 </script>
 <style lang="scss">
-
 </style>
